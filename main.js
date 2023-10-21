81375bad4916e2ca82222d7210393a05f0c21573
satyasmishra19 <143271875+satyasmishra19@users.noreply.github.com>
2023-10-21 18:30:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed('.text', {
+    strings:["photo editor","Gamer", "web designer","web Devloper"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backdelay:1000,
+    loop:true,
+});
+var typed = new Typed('.hello', {
+    strings:["Hello","Hy", "Namaste"],
+    typeSpeed: 100,
+    backspeed: 100,
+    backdelay:1000,
+    loop:true,
+});
